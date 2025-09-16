5ca3b91f1c3690eb397c74938717db8b1d5e71ea
jiyun0522 <wnlemforhs24@gmail.com>
2025-09-16 17:30:07 +0900

    ✨ Feat: RainbowColorBox 기능 수정

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,15 +1,23 @@
 import ColorBox from "./ColorBox";
 
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [color, setColor] = useState("");
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input
+          value={color}
+          onChange={(e) => {
+            setColor(e.target.value);
+          }}
+        />
       </div>
-      <ColorBox />
+      <ColorBox color={color} />
     </div>
   );
 }
