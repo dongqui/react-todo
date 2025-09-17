40985edb0d8476b0c148bf353423cb04d687afeb
양은지 <eunji124@github.com>
2025-09-17 15:17:58 +0900

    양은지 무지개 상자

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,15 +1,25 @@
 import ColorBox from "./ColorBox";
 
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // 인풋 박스 입력 값 받아와서 입력하기
+  const [inputValue, setInputValue] = useState('');
+
+  const HandleInput =(e) => {
+    setInputValue(e.target.value);
+  }
+  // 컬러박스에 bgc 에 값 전달하기
+
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input value={inputValue} onChange={HandleInput}/>
       </div>
-      <ColorBox />
+      <ColorBox color={inputValue}/>
     </div>
   );
 }
