c7ee2c4f0822c7f57627db5f50754f7a263764da
yujin-fe <hyujin1224@gmail.com>
2025-09-17 15:18:57 +0900

    레인보우 박스제작

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,15 +1,25 @@
-import ColorBox from "./ColorBox";
+//입력칸을 state로 받아서 state가 변하면 colorBox가 변해야함.
 
+import ColorBox from "./ColorBox";
+import {useState} from "react"
 import "./App.css";
 
+
+
 function App() {
+  const [input, setInput] = useState("white")
+
+  const onChangeInput = (e) => {
+    setInput(e.target.value)
+  }
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input onChange={onChangeInput}/>
       </div>
-      <ColorBox />
+      <ColorBox color={input}/>
     </div>
   );
 }
