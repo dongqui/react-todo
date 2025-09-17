4ab1a93d10a3c7f9f8dbece50cfae5312eeaac3e
양은지 <eunji124@github.com>
2025-09-17 15:35:51 +0900

    초기화 버튼 제작

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -2,24 +2,29 @@ import ColorBox from "./ColorBox";
 
 import "./App.css";
 import { useState } from "react";
+import ResetButton from "./ResetButton";
 
 function App() {
   // 인풋 박스 입력 값 받아와서 입력하기
   const [inputValue, setInputValue] = useState('');
 
-  const HandleInput =(e) => {
+  const handleInput =(e) => {
     setInputValue(e.target.value);
   }
-  // 컬러박스에 bgc 에 값 전달하기
 
+  //초기화 함수
+  const handleReset =() => {
+    setInputValue('');
+  }
 
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input value={inputValue} onChange={HandleInput}/>
+        <input value={inputValue} onChange={handleInput}/>
       </div>
       <ColorBox color={inputValue}/>
+      <ResetButton handleReset={handleReset} />
     </div>
   );
 }
---
+++ src/RainbowColorBox/ResetButton.jsx
@@ -0,0 +1,5 @@
+export default function ResetButton({handleReset}) {
+  return (
+    <button onClick={handleReset}>초기화 버튼</button>
+  );
+}
