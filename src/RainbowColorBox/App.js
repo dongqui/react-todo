ad11bd2429c5f0d55890cb5e9d16597e09c2729d
양은지 <eunji124@github.com>
2025-09-17 15:50:34 +0900

    에러 메세지 작업

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -3,6 +3,7 @@ import ColorBox from "./ColorBox";
 import "./App.css";
 import { useState } from "react";
 import ResetButton from "./ResetButton";
+import ErrorMessage from "./ErrorMessage";
 
 function App() {
   // 인풋 박스 입력 값 받아와서 입력하기
@@ -17,6 +18,17 @@ function App() {
     setInputValue('');
   }
 
+  //에러 메세지 : 무지개색 아닐 경우
+  const handleError = ({inputValue}) => {
+    //무지개색 배열로 정리
+    const rainbows = ['red', 'yellow', 'orange', 'green', 'blue', 'purple']
+    //무지개색이 아닐경우 함수 작동
+    if(inputValue.includes(rainbows)){
+      return ??
+      // 뭘 리턴 하는건지 모르겠음
+    }
+  }
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
@@ -25,6 +37,7 @@ function App() {
       </div>
       <ColorBox color={inputValue}/>
       <ResetButton handleReset={handleReset} />
+      <ErrorMessage />
     </div>
   );
 }
---
+++ src/RainbowColorBox/ErrorMessage.jsx
@@ -0,0 +1,5 @@
+export default function ErrorMessage({}) {
+  return (
+    <p>무지개 색이 아닙니다!</p>
+  );
+}
