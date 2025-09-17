62502d9cb02c1d08f9b9273fee671617c6e79eec
yujin-fe <hyujin1224@gmail.com>
2025-09-17 15:31:10 +0900

    초기화 버튼 추가

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,25 +1,30 @@
 //입력칸을 state로 받아서 state가 변하면 colorBox가 변해야함.
 
 import ColorBox from "./ColorBox";
+import ResetButton from "./ResetButton"
 import {useState} from "react"
 import "./App.css";
 
 
 
 function App() {
-  const [input, setInput] = useState("white")
+  const [color, setcolor] = useState("")
 
-  const onChangeInput = (e) => {
-    setInput(e.target.value)
+  const onChangeColor = (e) => {
+    setcolor(e.target.value)
+  }
+  const onClickDelete = () => {
+    setcolor("")
   }
 
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input onChange={onChangeInput}/>
+        <input value={color} onChange={onChangeColor}/>
       </div>
-      <ColorBox color={input}/>
+      <ColorBox color={color}/>
+      <ResetButton onClick={onClickDelete}/>
     </div>
   );
 }
---
+++ src/RainbowColorBox/ResetButton.jsx
@@ -0,0 +1,8 @@
+
+const ResetButton = ({onClick}) => {
+  return (
+    <button onClick={onClick}>초기화</button>
+  )
+}
+
+export default ResetButton
