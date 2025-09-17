73214bce7394f142d66c233f3aefaf5df92bae4e
jiyun0522 <wnlemforhs24@gmail.com>
2025-09-17 14:02:41 +0900

    📦 Chore: 삭제 버튼 기능 수정

--- src/TodoList/TodoItem.jsx
+++ src/TodoList/TodoItem.jsx
@@ -1,8 +1,8 @@
-export default function TodoItem({ todoItem, index, deleteTodo }) {
+export default function TodoItem({ todoItem, deleteTodo }) {
   return (
     <li>
       {todoItem.todo}
-      <button type="button" onClick={() => deleteTodo(index)}>
+      <button type="button" onClick={() => deleteTodo(todoItem.id)}>
         삭제
       </button>
       <button type="button">수정</button>
