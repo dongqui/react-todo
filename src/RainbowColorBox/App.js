0668d11e2b33033880ba94c17c9502229802012c
sylee <linggo01@naver.com>
2025-09-18 13:47:20 +0900

    에러메시지 작업중

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -3,13 +3,17 @@ import { useState } from "react";
 
 import "./App.css";
 import ResetButton from "./ResetButton";
+import ErrorMessage from "./ErrorMessage";
+
+const colors = ["red", "yellow", "green", "blue"];
 
 function App() {
   const [inputColor, setInputColor] = useState("");
+  const [isError, setIsError] = useState(false);
   const reset = () => {
     setInputColor("");
   };
-  console.log(inputColor);
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
@@ -18,11 +22,17 @@ function App() {
           onChange={(e) => {
             const value = e.target.value;
             setInputColor(value);
+
+            colors.filter((color) => {
+              color !== value;
+              return console.log(color);
+            });
           }}
         />
       </div>
       <ColorBox color={inputColor} />
       <ResetButton reset={reset} />
+      {isError && <ErrorMessage />}
     </div>
   );
 }
---
+++ src/RainbowColorBox/ErrorMessage.jsx
@@ -0,0 +1,5 @@
+function ErrorMessage() {
+  return <p>올바른 값이 아닙니다.</p>;
+}
+
+export default ErrorMessage;
