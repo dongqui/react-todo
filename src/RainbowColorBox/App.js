afef0892392fdb2987db6737694ad955ab865992
sylee <linggo01@naver.com>
2025-09-18 13:12:00 +0900

    ColorBox 작업완료

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,15 +1,22 @@
 import ColorBox from "./ColorBox";
+import { useState } from "react";
 
 import "./App.css";
 
 function App() {
+  const [inputColor, setInputColor] = useState("");
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input
+          onChange={(e) => {
+            const value = e.target.value;
+            setInputColor(value);
+          }}
+        />
       </div>
-      <ColorBox />
+      <ColorBox color={inputColor} />
     </div>
   );
 }
