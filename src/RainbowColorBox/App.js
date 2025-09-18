0fec57c522a97fcdeac08081252fb958c6018e11
Choi hee ruk <chlgmlfkr593@naver.com>
2025-09-18 15:44:13 +0900

    rainbow

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,15 +1,16 @@
 import ColorBox from "./ColorBox";
-
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [rainbow, setRainbow] = useState("");
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
         <input />
       </div>
-      <ColorBox />
+      <ColorBox color={rainbow} />
     </div>
   );
 }
