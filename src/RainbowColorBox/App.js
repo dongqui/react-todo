8ec30fd7e104e283e0317c384cc70889b9974cfb
k0nghaa <k0nghaalee@gmail.com>
2025-09-19 14:04:06 +0900

    feat: RainbowColorbox 기능 구현 시도
    첫번째 시도

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,15 +1,30 @@
 import ColorBox from "./ColorBox";
+import { useState } from "react";
 
 import "./App.css";
 
 function App() {
+  const [inputText, setInputText] = useState("red");
+  const [color, setColor] = useState();
+
+  const onChangeInput = (e) => {
+    console.log(e.target.value);
+    setInputText(e.target.value);
+  };
+
+  const onChangeColor = (e) => {
+    if (String(inputText) === String(color)) {
+      setColor(ColorBox);
+    }
+  };
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input type={inputText} onChange={onChangeInput} />
       </div>
-      <ColorBox />
+      <ColorBox color={color} onChange={onChangeColor} />
     </div>
   );
 }
