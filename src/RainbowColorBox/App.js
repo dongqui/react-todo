0bedbd00185ac876c051c45664b0ca237838518f
apple <apple@apples-MacBook-Air.local>
2025-09-19 21:36:23 +0900

    연습

--- src/RainbowColorBox/App.js
+++ src/RainbowColorBox/App.js
@@ -1,15 +1,30 @@
 import ColorBox from "./ColorBox";
-
+import {useState} from 'react'
+import ResetButton from "./ResetButton";
 import "./App.css";
 
+
+
 function App() {
+
+  const [inputText,setInputText] = useState('red');
+
+  const onChangeInput = (e) => {
+  setInputText(e.target.value)
+}
+
+  const resetAll = () => {
+    setInputText('')
+  }
+
   return (
     <div className="App">
       <h1>무지개색 상자</h1>
       <div>
-        <input />
+        <input value={inputText} onChange={onChangeInput}/>
       </div>
-      <ColorBox />
+      <ColorBox color={inputText}/>
+      <ResetButton onClick={resetAll}/>
     </div>
   );
 }
