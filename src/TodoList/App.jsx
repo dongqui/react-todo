b5781eca806e97b03d3e230409cbd6b413b22d34
yujin-fe <hyujin1224@gmail.com>
2025-09-23 11:04:26 +0900

    reducer 함수 추가

--- src/TodoList/App.jsx
+++ src/TodoList/App.jsx
@@ -3,71 +3,95 @@
 //todoInput의 입력 버튼을 누르면 state를 todoItem 컴포넌트에 전달/노출
 import TodoInput from "./TodoInput";
 import TodoItem from "./TodoItem";
-import {useState, useEffect} from "react";
+import {useEffect, useReducer } from "react";
+
+function reducer(todos, action) {
+  switch (action.type) {
+    case "INIT":
+      return action.data;
+    case "CREATE":
+      return [action.data, ...todos];
+    case "UPDATE": return todos.map((item) => (item.id === action.data.id ? action.data : item))
+    case "DELETE": return todos.filter((item)=>(item.id!==action.data.id))
+    default:
+      return todos;
+  }
+}
 
 export default function App() {
-  const [list, setList] = useState([]);
+  const [todos, dispatch] = useReducer(reducer, []);
+  // const [list, setList] = useState([]);
   // const idRef = useRef(1);
 
-  useEffect(()=>{
-    async function getData (){
-      const res = await fetch('/todos');
+  useEffect(() => {
+    async function getData() {
+      const res = await fetch("/todos");
       const todos = await res.json();
 
-      setList(todos);
+      dispatch({
+        type: "INIT",
+        data: todos,
+      });
+      // setList(todos);
     }
     getData();
-  },[])
+  }, []);
 
-  const onClickCreate = async (input) =>{
-    const res = await fetch('/todos',{
-      method:"POST",
-      body:JSON.stringify({title:input})
-    })
-    const todo = await res.json()
-    setList([
-      todo,
-      ...list,
-    ]);
-  }
-
-  const onClickDelete = async (id) => {
-    const res = await fetch(`/todos/${id}`,{
-      method:"DELETE",
-    })
-    const todo = await res.json()
-
-    setList(list.filter((item)=>
-      item.id!==todo.id
-    ))
-  }
-
-  const onClickUpdate = async (id, editedContent) => {
-    const res = await fetch(`/todos/${id}`,{
-      method:"PATCH",
-      body:JSON.stringify({
-        title:editedContent
-      })
+  const onClickCreate = async (input) => {
+    const res = await fetch("/todos", {
+      method: "POST",
+      body: JSON.stringify({ title: input }),
     });
     const todo = await res.json();
+    dispatch({
+      type: "CREATE",
+      data: todo,
+    });
+    // setList([
+    //   todo,
+    //   ...list,
+    // ]);
+  };
 
-    setList(list.map((item)=>
-      item.id===todo.id
-      ?todo
-      :item
-    ))
-  }
+  const onClickUpdate = async (id, editedContent) => {
+    const res = await fetch(`/todos/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        title: editedContent,
+      }),
+    });
+    const todo = await res.json();
+    dispatch({
+      type:"UPDATE",
+      data:todo,
+    })
+    // setList(list.map((item) => (item.id === todo.id ? todo : item)));
+  };
+
+  const onClickDelete = async (id) => {
+    const res = await fetch(`/todos/${id}`, {
+      method: "DELETE",
+    });
+    const todo = await res.json();
+    dispatch({
+      type:"DELETE",
+      data:todo
+    })
+    // setList(list.filter((item) => item.id !== todo.id));
+  };
 
   return (
     <div>
-      <TodoInput onClick={onClickCreate}/>
+      <TodoInput onClick={onClickCreate} />
       <ul>
-        {list.map((item)=><TodoItem 
-          key={item.id} 
-          onClickDelete={onClickDelete} 
-          onClickUpdate={onClickUpdate} 
-          item={item}
-        />)}
+        {todos.map((item) => (
+          <TodoItem
+            key={item.id}
+            onClickDelete={onClickDelete}
+            onClickUpdate={onClickUpdate}
+            item={item}
+          />
+        ))}
       </ul>
     </div>
   );
